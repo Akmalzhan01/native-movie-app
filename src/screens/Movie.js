a66ac838deef2a085347e4febb986cc03583d89b
Akmalzhan <akmalzhantokhtasinov@gmail.com>
2023-12-11 14:48:07 +0600

    #11 Movie api detail

--- src/screens/Movie.js
+++ src/screens/Movie.js
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
-import { View, ScrollView, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { View, ScrollView, TouchableOpacity, Dimensions, Image, Text } from 'react-native';
 import { ChevronLeftIcon } from "react-native-heroicons/outline"
 import { HeartIcon } from "react-native-heroicons/solid"
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -63,18 +63,37 @@ export default function Movie() {
           <View>
             <Image source={{ uri: image500(movie.poster_path) }}
               style={{ width, height: height * 0.5 }} />
-              <LinearGradient 
+            <LinearGradient
               colors={[
                 "transparent",
                 "rgba(23,23,23, 0.8)",
                 "rgba(23,23,23, 1)",
-              ]} 
-              style={{width, height: height * 0.4}}
-              start={{x: 0.5, y: 0}}
-              end={{x: 0.5, y: 1}}
+              ]}
+              style={{ width, height: height * 0.4 }}
+              start={{ x: 0.5, y: 0 }}
+              end={{ x: 0.5, y: 1 }}
               className="absolute bottom-0" />
           </View>
         )}
+        <View className="space-y-4" style={{ marginTop: -40 }}>
+          <Text className="text-white text-center text-3xl font-bold tracking-widest">{movie?.title}</Text>
+          {movie?.id && (
+            <Text className="text-neutral-400 font-semibold text-base text-center">
+              {movie?.status} •
+              {movie?.release_date?.split("-")[0]} • {" "}
+              {movie?.runtime} min
+            </Text>
+          )}
+          <View className="flex-row justify-center items-center mx-4 space-x-2">
+            {movie?.genres?.map((genre, idx) => (
+              <Text key={idx} className="text-neutral-400 font-semibold text-base text-center">
+                {genre?.name} {" "}
+                {idx + 1 !== movie.genres.length ? "•" : null}
+              </Text>
+            ))}
+          </View>
+        </View>
+        <Text className="text-neutral-400 mx-4 tracking-wide">{movie?.overview}</Text>
       </View>
     </ScrollView>
   )
